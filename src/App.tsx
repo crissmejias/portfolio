533412e389c2a51b5f04a18e51733b2eeb93e152
Cristhian Mejias Serrano <cristhianmejias495@gmail.com>
2023-02-10 18:07:57 -0500

    Componentes Sidebar y Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { Header } from "./components/Header";
 import { Routes, Route } from "react-router-dom";
+import { Sidebar } from "./components/Sidebar";
+import { Home } from "./components/Home";
 const App = ()  => {
 return(
     <>
     <Header/>
+    <div className="w-full h-screen grid grid-rows-[1fr_1fr] md:grid-cols-[450px_1fr] md:grid-rows-1 bg-gray-900">
+        <Sidebar/>
         <Routes>
-        <Route path="/"/>
+        <Route path="/" element={<Home/>}/>
         <Route path="/projects"/>
         <Route path="/contact"/>
         </Routes>
+    </div>
     </>
 )
 }
---
+++ src/components/Home.tsx
@@ -0,0 +1,6 @@
+const Home = () => {
+return(
+    <div className="border-2 w-11/12 mt-4 rounded-lg mx-auto bg-gray-700"></div>
+)
+}
+export {Home};
